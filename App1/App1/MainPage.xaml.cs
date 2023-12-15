826939a8058c598cc84da0989635fc718883f8e0
Nathan Silva <nathan.silva26@gmail.com>
2023-12-15 13:31:29 -0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using App1.Service;
+using App1.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Button.Clicked += BuscarCEP;
+        }
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+            if (IsValidCEP(cep))
+            {
+                try
+                {
+                    Endereco endereco = ViaCepService.BuscarEnderecoViaCep(cep);
+
+                    if (endereco == null)
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado", "Ok");
+                    else
+                        Result.Text = string.Format("Endereco: {2} de {3} {0}, {1} ", endereco.localidade, endereco.uf, endereco.logradouro, endereco.bairro);
+
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO", ex.Message, "OK");
+                }
+            }
+            else
+            {
+                DisplayAlert("CEP INVÁLIDO", "A quantidade de dígitos do CEP é inválida", "OK");
+
+            }
+
+        }
+        private bool IsValidCEP(string cep)
+        {
+            bool valid = false;
+            if (cep.Length != 8)
+                return valid;
+            return valid = true;
+        }
+    }
+}
---
+++ App1/App1/Service/ViaCepService.cs
@@ -0,0 +1,27 @@
+﻿using App1.Service.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace App1.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(content);
+            
+            if (endereco.cep == null) return null;
+
+            return endereco;
+        }
+    }
+}
